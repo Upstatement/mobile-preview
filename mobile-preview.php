cf8641a0152053d15c8176491c128a25231642f7
Jared Novack <jared@upstatement.com>
2014-09-26 14:18:15 -0500

    added preview to admin view

--- mobile-preview.php
+++ mobile-preview.php
@@ -4,7 +4,7 @@ Plugin Name: Mobile Preview
 Plugin URI: http://upstatement.com/mobile-preview
 Description: Preview your site in a variety of mobile devices
 Author: Jared Novack + Upstatement
-Version: 0.0.1
+Version: 0.0.2
 Author URI: http://upstatement.com/
 */
 
@@ -13,11 +13,11 @@ class Upstatement_MobilePreview {
 	function __construct() {
 		$this->load_dependencies();
 		$this->add_toolbar_action();
-		if (!is_admin()) {
-			$this->detect_viewed_in_iframe();
-			$this->inject_html();
-			add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_static' ) );
-		}
+		$this->detect_viewed_in_iframe();
+		add_action( 'wp_footer', array($this, 'inject_html'));
+		add_action( 'admin_footer', array($this, 'inject_html'));
+		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_static' ) );
+		add_action( 'admin_enqueue_scripts', array($this, 'enqueue_static') );
 	}
 
 	function load_dependencies() {
@@ -27,16 +27,31 @@ class Upstatement_MobilePreview {
 	}
 
 	function add_toolbar_action() {
-		if ( !is_admin() ) {
-			Jigsaw::add_admin_bar_item( 'Mobile Preview', '#mobile-preview' );
-		}
+		Jigsaw::add_admin_bar_item( 'Mobile Preview', '#mobile-preview' );
 	}
 
 	function inject_html() {
-		add_action('wp_footer', function(){
-			require_once 'mobile-preview-window.twig';
-		}, 10);
+		$iframe_url = self::get_current_url();
+		if (is_admin()) {
+			global $post;
+			$iframe_url = home_url('/?p='.$post->ID);
+		}
+		$iframe_url = add_query_arg('admin_bar', 'false', $iframe_url);
+		require_once 'mobile-preview-window.twig';
 	}
+
+	public static function get_current_url() {
+        $pageURL = "http://";
+        if (isset($_SERVER['HTTPS']) && $_SERVER["HTTPS"] == "on") {
+            $pageURL = "https://";;
+        }
+        if ($_SERVER["SERVER_PORT"] != "80") {
+            $pageURL .= $_SERVER["SERVER_NAME"] . ":" . $_SERVER["SERVER_PORT"] . $_SERVER["REQUEST_URI"];
+        } else {
+            $pageURL .= $_SERVER["SERVER_NAME"] . $_SERVER["REQUEST_URI"];
+        }
+        return $pageURL;
+    }
 
 	function enqueue_static() {
 		wp_enqueue_style( 'mobile-preview-styles', plugins_url( '/css/screen.css', __FILE__ ) );
